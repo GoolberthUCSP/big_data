a6b6a0fd9c5300cc9e6f384c2b1bccd3c3b3a6ec
Fredy Goolberth Quispe Neira <fredy.quispe@ucsp.edu.pe>
2024-09-04 20:14:30 -0500

    Processing with threadpool added

--- exercises/gen_files.cpp
+++ exercises/gen_files.cpp
@@ -1,22 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 #include <thread>
+#include <chrono>
+#include <random>
 #include <windows.h>
+#include "threadpool.h"
 
 using namespace std;
 
-const int N_FILES = 1000;
-string SIZE_ = "10000"; // in Mb
+const long int N_FILES = 1000;
+const long int POOL_SIZE = thread::hardware_concurrency();
+const long int DATA_SIZE = 20000; // in Mb
+vector<string> words;
+int words_size;
 string DIR = "data/inv_idx/";
 
+void create_file(string filename, long int n_words);
+
 int main(int argc, char *argv[]) {
-    string cmd = "g++ -o gen_file.exe gen_file.cpp";
-    system(cmd.c_str());
-    string file_size = to_string(atoi(SIZE_.c_str()) / N_FILES);
+    srand(time(NULL));
+    long int file_size = DATA_SIZE / N_FILES;
+    long int n_words = file_size * 100000;
+
+    ThreadPool pool(POOL_SIZE);
+    vector<future<void>> futures;
+
+    // Load dictionary    
+    ifstream dict("data/dict.txt");
+    string line;
+    while (getline(dict, line)) {
+        words.push_back(line);
+    }
+    dict.close();
+    words_size = words.size();
+
+    auto start = chrono::high_resolution_clock::now();
     for (int i = 0; i < N_FILES; i++) {
         string filename = DIR + "file_" + to_string(i) + ".txt";
-        cmd = "gen_file.exe " + filename + " " + file_size;
-        system(cmd.c_str());
+        futures.emplace_back(
+            pool.enqueue([filename, n_words] { create_file(filename, n_words); })
+        );
     }
+    for (int i = 0; i < N_FILES; i++) {
+        futures[i].get();
+    }
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end - start;
+    cout << "Elapsed time creating " << N_FILES << " files of " << file_size << " Mb each: " << elapsed.count() << " seconds" << endl;
+}
+
+void create_file(string filename, long int n_words) {
+    ofstream fout(filename);
+    for (long int i = 0; i < n_words; i++) {
+        fout << words[rand() % words_size] << " ";
+    }
+    fout.close();
 }
--- exercises/inverted_index.cpp
+++ exercises/inverted_index.cpp
@@ -8,13 +8,16 @@
 #include <chrono>
 #include <utility>
 #include <unordered_map>
+#include "threadpool.h"
 
 using namespace std;
 
 const int N_FILES = 1000;
-const long long int N_THREADS = 32; // thread::hardware_concurrency();
+const long long int N_THREADS = 32;
 const long long int BUFFER_SIZE = 512 * 1024;
+const int POOL_SIZE = thread::hardware_concurrency();
 
+void linear_word_counter(string filename, unordered_map<string, long long int> &result);
 void word_counter(string filename, unordered_map<string, long long int> &result);
 void process_chunk(string filename, long long int start, long long int end, unordered_map<string, long long int> &counter);
 string DIR = "data/inv_idx/";
@@ -32,28 +35,53 @@ vector<string> split(string s, char delimiter) {
 int main() {
     setlocale(LC_ALL, "es_CO.UTF-8");
     vector<unordered_map<string, long long int>> word_counts(N_FILES);
-
+    
+    ThreadPool pool(POOL_SIZE);
+    vector<future<void>> futures;
+    // PROCESS WORD COUNTS
     auto start = chrono::high_resolution_clock::now();
     for (int i = 0; i < N_FILES; i++) {
         string filename = DIR + "file_" + to_string(i) + ".txt";
-        word_counter(filename, word_counts[i]);
+        futures.emplace_back(
+            pool.enqueue([filename, &word_counts, i] {
+                linear_word_counter(filename, word_counts[i]);
+            })
+        );
+    }
+    for (int i = 0; i < N_FILES; i++) {
+        futures[i].get();
     }
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed = end - start;
-
     cout << "Elapsed time processing word_counts: " << elapsed.count() << " seconds" << endl;
+    // PROCESS INVERTED INDEX
+    unordered_map<string, vector<pair<int, long long int>>> inverted_index;
+    start = chrono::high_resolution_clock::now();
+    for (int i = 0; i < N_FILES; i++) {
+        for (auto it = word_counts[i].begin(); it != word_counts[i].end(); it++) {
+            inverted_index[it->first].push_back({i, it->second});
+        }
+    }
+    end = chrono::high_resolution_clock::now();
+    elapsed = end - start;
 
-    // unordered_map<string, vector<pair<int, long long int>>> inverted_index;
-    // start = chrono::high_resolution_clock::now();
-    // for (int i = 0; i < N_FILES; i++) {
-    //     for (auto it = word_counts[i].begin(); it != word_counts[i].end(); it++) {
-    //         inverted_index[it->first].push_back({i, it->second});
-    //     }
-    // }
-    // end = chrono::high_resolution_clock::now();
-    // elapsed = end - start;
+    cout << "Elapsed time processing inverted_index: " << elapsed.count() << " seconds" << endl;
+}
 
-    // cout << "Elapsed time processing inverted_index: " << elapsed.count() << " seconds" << endl;
+void linear_word_counter(string filename, unordered_map<string, long long int> &result) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cout << "Error opening file" << endl;
+        exit(1);
+    }
+
+    stringstream ss;
+    ss << file.rdbuf();
+    string word;
+    while (ss >> word) {
+        result[word]++;
+    }
+    file.close();
 }
 
 void word_counter(string filename, unordered_map<string, long long int> &result) {
